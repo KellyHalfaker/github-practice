8173531e805f800bda6439514c0e7130d8879a54
Kelly Halfaker <halfakerkelly@gmail.com>
2019-02-15 11:25:23 -0800

    solved oddOrEven

--- oddOrEven.js
+++ oddOrEven.js
@@ -1,9 +1,13 @@
 // Update the following function to take an argument and output 'Even' if the argument is an even number or 'Odd' if the number is an odd number.
 
-function oddOrEven(){
-    console.log('result')
+function oddOrEven(num) {
+  if (num % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
 }
 
-oddOrEven(4)
-oddOrEven(11)
-oddOrEven(75)
+console.log(oddOrEven(4));
+console.log(oddOrEven(11));
+console.log(oddOrEven(75));
